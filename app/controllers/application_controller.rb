cc4520e32282667d6fd5c61d4f1e6d153cb6763f
Steven Haws <stevenjhaws@gmail.com>
2018-02-28 19:03:28 -0700

    added game view and application controller error method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def render_error(model)
+    render json: {errors: model.errors.full_message.join(",")}, status: 422
+  end
 end
